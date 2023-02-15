03c48e273654406ba26e2454cb70fe34b7b9f446
SuryaxMattoo <114922647+SuryaxMattoo@users.noreply.github.com>
2023-02-14 21:44:37 -0500

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,11 @@
+public class Game {
+    static int diffLvl;
+    static int finalScore;
+
+    Game (int diffLvl, int finalScore){
+        this.diffLvl = diffLvl;
+        this.finalScore = finalScore;
+    }
+
+}
+
